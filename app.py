b1d5516dcfe97b339c946dad7eef1114d06b1985
sudhanshuojhs2009 <40880872+sudhanshuojhs2009@users.noreply.github.com>
2019-01-29 10:43:20 +0530

    updated heading variable

--- app.py
+++ app.py
@@ -4,30 +4,33 @@ from jinja2 import Environment, FileSystemLoader
 from eqdata import show_data, write_data, search
 env = Environment(loader=FileSystemLoader('html'))
 
+heading = write_data()
+
 
 class EquityTopTEn(object):
     @cherrypy.expose
     def index(self):
         eqdata = show_data()
-        heading = write_data()
+        global heading
         tmpl = env.get_template('index.html')
         return tmpl.render(heading=heading, eqdata=eqdata)
 
     @cherrypy.expose
     def generate(self, q):
         result = search(q)
-        heading = write_data()
+        global heading
         tmpl = env.get_template('search.html')
         return tmpl.render(result=result, heading=heading)
 
 
 if __name__ == '__main__':
-    cherrypy.engine.housekeeper = cherrypy.process.plugins.BackgroundTask(21600, write_data()) # needs to be set at market refresh time
-    cherrypy.engine.housekeeper.start()
+    #write_data()
+    # cherrypy.engine.housekeeper = cherrypy.process.plugins.BackgroundTask(21600, write_data())
+    # cherrypy.engine.housekeeper.start()
     config = {
         'global': {
             'server.socket_host': '0.0.0.0',
-            'server.socket_port': int(os.environ.get('PORT', 80)),
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
         },
         '/assets': {
             'tools.staticdir.root': os.path.dirname(os.path.abspath(__file__)),
